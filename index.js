a667773b218f8d26f61e79378837b35d7fec41b4
elmeri kuismin <elmeri.kuismin@gmail.com>
2020-03-10 16:58:25 +0200

    Initiate app.

--- index.js
+++ index.js
@@ -1,10 +1,83 @@
-const http = require('http')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
-const app = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' })
-  res.end('Hello World')
+app.use(bodyParser.json())
+app.use(cors())
+
+let persons = [
+      {
+        "name": "Arto Järvinen",
+        "number": "040-123456",
+        "id": 1
+      },
+      {
+        "name": "Big bertha",
+        "number": "1235234",
+        "id": 2
+      },
+      {
+        "name": "Petteri Aaltonen",
+        "number": "12352344123",
+        "id": 5
+      }
+]
+
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
 })
 
-const port = 3001
-app.listen(port)
-console.log(`Server running on port ${port}`)
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+const generateId = () => {
+  const random = Math.floor(Math.random() * 10000); 
+  return random
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (body.name === undefined || body.number === undefined) {
+    return response.status(400).json({ error: 'content missing' })
+  }
+  if (persons.find(person => person.name === body.name)){
+    return response.status(400).json({ error: 'name must be unique' })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId()
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
